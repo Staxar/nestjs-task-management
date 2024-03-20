a854b2f0e4e485a9adb35e34ec57f01141eabac6
madomaga <marcin.domagala@capgemini.com>
2024-03-20 13:11:51 +0100

    Add validation

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
 import { TaskStatus } from '../tasks.model';
-
+import { IsOptional, IsEnum, IsString } from 'class-validator';
 export class getTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+  @IsOptional()
+  @IsString()
   search?: string;
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -30,7 +30,13 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Could not find Task with ${id}`);
+    }
+
+    return found;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -46,6 +52,7 @@ export class TasksService {
   }
 
   deleteTaskById(id: string): void {
+    const found = this.getTaskById(id);
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
